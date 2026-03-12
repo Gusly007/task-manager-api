88740eddb0e44d6719a7f28d410400b2b44152a6
gusly007 <jngusly@gmzil.com>
2026-03-12 10:28:25 +0100

    feat: add Express app setup and server entry point

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+const errorMiddleware = require('./middlewares/errorMiddleware');
+const setupSwagger = require('./docs/swagger');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+setupSwagger(app);
+
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
+
+app.use(errorMiddleware);
+
+module.exports = app;
